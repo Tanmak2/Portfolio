08840571afae41a92f066e697b908a59d5f0897c
Tanmak2 <whdcks3131@naver.com>
2022-11-13 21:51:46 +0900

    feat : 타이틀 그림자 수정

--- static/game/BlackJack/static/js/BlackJack.js
+++ static/game/BlackJack/static/js/BlackJack.js
@@ -1,7 +1,1 @@
 var card = document.getElementsByClassName('card');
-
-for (var i = 0; i < card.length; i++) {
-  card[i].addEventListener('click', function () {
-    card[i].setAttribute('style', 'transform: rotateY(180deg)');
-  });
-}
